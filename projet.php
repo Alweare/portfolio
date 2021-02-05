61a458c654b8518daac651b62578c6d18f64054c
Alweare <yanis.dumont@gmail.com>
2021-02-05 16:26:27 +0100

    Création page projet

---
+++ projet.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/67f19573a2.js" crossorigin="anonymous"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+    </style>
+    <title>Yanis Dumont</title>
+</head>
+
+<body>
+    <!-- Insertion du header -->
+    <?php include '_header.php'; ?>
+
+    <div class="cadre_presentation">
+
+        <h1 class="titre_projet">
+            Mes projets
+        </h1>
+    </div>
+    <div id="liste_projet">
+        <div class="projet">
+            <img src="images/chaletetcaviar.png" alt="projet_chalet_et_caviar" />
+
+
+        </div>
+        <div class="projet">
+            <img src="images/chaletetcaviar.png" alt="projet_chalet_et_caviar" />
+
+
+        </div>
+    </div>
+
+
+
+    <!-- Insertion du footer -->
+
+    <?php include '_footer.php' ?>
+
+</body>
+
+</html>
